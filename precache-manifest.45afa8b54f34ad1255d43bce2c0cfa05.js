ec95f61f1d55cc771d15963bcb64c5f28803febe
Tianlei Xu <tiaxu@microsoft.com>
2020-01-03 11:31:29 -0800

    Updates

---
+++ precache-manifest.45afa8b54f34ad1255d43bce2c0cfa05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ea9e5608492533c36638bcceccb3ce",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "db7d74f1a73559d48886",
+    "url": "/contactmanager/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "e8ec964387ef99dad4c4",
+    "url": "/contactmanager/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "db7d74f1a73559d48886",
+    "url": "/contactmanager/static/js/2.f32cb14f.chunk.js"
+  },
+  {
+    "revision": "c8336e0bcbf8ea1670aafb15aa044bf7",
+    "url": "/contactmanager/static/js/2.f32cb14f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e8ec964387ef99dad4c4",
+    "url": "/contactmanager/static/js/main.3e233651.chunk.js"
+  },
+  {
+    "revision": "4a0c35d631389aa11b2d",
+    "url": "/contactmanager/static/js/runtime-main.d144d510.js"
+  }
+]);
